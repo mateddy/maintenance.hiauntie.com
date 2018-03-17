cc8476ecb560567260ba98e823960ada8811e64a
Luzi82 <luzi82@gmail.com>
2018-03-17 15:35:30 +0800

    full hiauntie-mastodon backup script

--- hiauntie/script/backup.mastodon.sh
+++ hiauntie/script/backup.mastodon.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
+# refer to https://github.com/tootsuite/documentation/blob/master/Maintaining-Mastodon/Backups-Guide.md
+
 set -e
 
-mkdir -p /tmp/XFJXIDUJ-backup
+rm -rf /tmp/XFJXIDUJ-backup
+
+# backup DB
 pg_dump mastodon_production > /tmp/XFJXIDUJ-backup/mastodon_production.sql
+
+# backup user generated content
+cd /home/mastodon/live/public
+tar -czf /tmp/XFJXIDUJ-backup/ugc.tar.gz system
+
+# backup server secret
+cp /home/mastodon/live/.env.production /tmp/XFJXIDUJ-backup/
