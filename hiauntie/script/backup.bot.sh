433f7e15066f03cc8ba6bbbd05a713ceeee1751b
Luzi82 <luzi82@gmail.com>
2018-03-17 22:08:21 +0800

    avoid disk usage peak

--- hiauntie/script/backup.bot.sh
+++ hiauntie/script/backup.bot.sh
@@ -10,6 +10,7 @@ TIMESTAMP_FULL=`date +%Y%m%d-%H%M%S`
 echo JEJJYMOF TIMESTAMP_FULL=${TIMESTAMP_FULL}
 
 echo KDSVWOCI create backup files
+cd /tmp
 sudo rm -rf /tmp/XFJXIDUJ-backup
 sudo sudo -u mastodon /home/hiauntie_bot/maintenance.hiauntie.com/hiauntie/script/backup.mastodon.sh
 sudo chown -R hiauntie_bot:hiauntie_bot /tmp/XFJXIDUJ-backup
@@ -17,13 +18,13 @@ sudo chown -R hiauntie_bot:hiauntie_bot /tmp/XFJXIDUJ-backup
 echo LEEIMVXB tar gz everything
 cd /tmp/XFJXIDUJ-backup
 tar -czf hiauntie-backup.tar.gz hiauntie-backup
+rm -rf /tmp/XFJXIDUJ-backup/hiauntie-backup
 
 echo COZNCIRT encrypt files
 cd /tmp/XFJXIDUJ-backup
 pwgen -s 256 1 > /tmp/XFJXIDUJ-backup/enc.key
 openssl enc -aes-256-cbc -salt -kfile /tmp/XFJXIDUJ-backup/enc.key -in hiauntie-backup.tar.gz -out hiauntie-backup.tar.gz.enc
 openssl enc -aes-256-cbc -salt -kfile /home/hiauntie_bot/.hiauntie/backup_enc_key -in /tmp/XFJXIDUJ-backup/enc.key -out /tmp/XFJXIDUJ-backup/enc.key.enc
-rm -rf /tmp/XFJXIDUJ-backup/hiauntie-backup
 rm -rf /tmp/XFJXIDUJ-backup/hiauntie-backup.tar.gz
 rm -rf /tmp/XFJXIDUJ-backup/enc.key
 
@@ -38,6 +39,8 @@ rm -rf /tmp/XFJXIDUJ-backup/pack
 echo KXYMXFAE upload to s3
 cd /tmp/XFJXIDUJ-backup
 aws s3 cp /tmp/XFJXIDUJ-backup/pack.tar s3://hiauntie-backup/data/${TIMESTAMP_YYYY}/${TIMESTAMP_FULL}/data-backup-${TIMESTAMP_FULL}.tar --quiet
+rm -rf /tmp/XFJXIDUJ-backup/pack.tar
 
 echo GQNLIDYQ clean up
+cd /tmp
 sudo rm -rf /tmp/XFJXIDUJ-backup
