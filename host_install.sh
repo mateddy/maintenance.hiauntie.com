37ab2070930d07b87f4a618bc4aee12a77ee08cb
Luzi82 <luzi82@gmail.com>
2018-03-17 14:50:00 +0800

    fix exit err

--- host_install.sh
+++ host_install.sh
@@ -7,12 +7,12 @@ MY_HOSTNAME=`hostname`
 
 if [ "${MY_DNSDOMAINNAME}" != "luzi82.com" ]; then
     echo run me only on hiauntie_bot@host0.luzi82.com
-    return 1
+    exit 1
 fi
 
 if [ "${MY_HOSTNAME}" != "host0" ]; then
     echo run me only on hiauntie_bot@host0.luzi82.com
-    return 1
+    exit 1
 fi
 
 scp hiauntie/script/hiauntie_run.sh hiauntie_bot@hiauntie.com:/home/hiauntie_bot/
