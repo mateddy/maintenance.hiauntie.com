dec90f2b0c2a09aee6187d5f2dd6d517ff5fbfab
Luzi82 <luzi82@gmail.com>
2018-03-21 01:07:26 +0800

    do not die when log fail

--- host/script/backup.sh
+++ host/script/backup.sh
@@ -7,11 +7,11 @@ TIMESTAMP_FULL=`date +%Y%m%d-%H%M%S`
 echo UFIEXGNO backup start ${TIMESTAMP_FULL}
 
 cd /home/hiauntie_bot/maintenance.hiauntie.com
-python3 -m hiauntie_py.toot verbose "URLKLBPN backup ${TIMESTAMP_FULL} start"
+python3 -m hiauntie_py.toot verbose "URLKLBPN backup ${TIMESTAMP_FULL} start" || true
 
 ssh hiauntie_bot@hiauntie.com -C /home/hiauntie_bot/maintenance.hiauntie.com/hiauntie/script/backup.bot.sh
 
 cd /home/hiauntie_bot/maintenance.hiauntie.com
-python3 -m hiauntie_py.toot verbose "HHNSHGUM backup ${TIMESTAMP_FULL} end"
+python3 -m hiauntie_py.toot verbose "HHNSHGUM backup ${TIMESTAMP_FULL} end" || true
 
 echo JXDNKIBA backup end ${TIMESTAMP_FULL}
