f5072aecb41e05f3fd3d20d1d3bfff6d48f7e9e3
Luzi82 <luzi82@gmail.com>
2018-03-21 01:35:25 +0800

    toot it is data backup

--- host/script/backup.sh
+++ host/script/backup.sh
@@ -7,11 +7,11 @@ TIMESTAMP_FULL=`date +%Y%m%d-%H%M%S`
 echo UFIEXGNO backup start ${TIMESTAMP_FULL}
 
 cd /home/hiauntie_bot/maintenance.hiauntie.com
-python3 -m hiauntie_py.toot --account verbose --msg "URLKLBPN backup ${TIMESTAMP_FULL} start" || true
+python3 -m hiauntie_py.toot --account verbose --msg "URLKLBPN data backup ${TIMESTAMP_FULL} start" || true
 
 ssh hiauntie_bot@hiauntie.com -C /home/hiauntie_bot/maintenance.hiauntie.com/hiauntie/script/backup.bot.sh
 
 cd /home/hiauntie_bot/maintenance.hiauntie.com
-python3 -m hiauntie_py.toot --account verbose --msg "HHNSHGUM backup ${TIMESTAMP_FULL} end" || true
+python3 -m hiauntie_py.toot --account verbose --msg "HHNSHGUM data backup ${TIMESTAMP_FULL} end" || true
 
 echo JXDNKIBA backup end ${TIMESTAMP_FULL}
