1db865e2d7d6d3055c804b0769736cf3f6f6c6bc
Luzi82 <luzi82@gmail.com>
2018-03-20 23:29:26 +0800

    add toot bot script

---
+++ hiauntie_py/toot.py
@@ -0,0 +1,20 @@
+import argparse
+from mastodon import Mastodon
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--account', type=str, choices=['announcement','verbose'])
+parser.add_argument('--msg', type=str)
+parse_args = parser.parse_args()
+
+if not os.path.exists('/home/hiauntie_bot/.hiauntie/config.json'):
+    print('UAOWOLXS /home/hiauntie_bot/.hiauntie/config.json not exist')
+    sys.exit(1)
+
+user_secret_file = '/home/hiauntie_bot/.hiauntie/user_{}.secret'.format(parse_args.account)
+
+mastodon = Mastodon(
+    client_id = '/home/hiauntie_bot/.hiauntie/bot_client.secret',
+    access_token = user_secret_file,
+    api_base_url = 'https://hiauntie.com'
+)
+mastodon.toot(parse_args.msg)
