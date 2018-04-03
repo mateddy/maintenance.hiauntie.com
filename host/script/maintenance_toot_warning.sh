1017b64861ddc6e79bdb1cba0318a7185e03dc02
Luzi82 <luzi82@gmail.com>
2018-04-04 00:08:20 +0800

    finalize

--- host/script/maintenance_toot_warning.sh
+++ host/script/maintenance_toot_warning.sh
@@ -5,6 +5,6 @@ set -e
 echo "YBYXNLCG maintenance_toot_warning toot start"
 
 cd /home/hiauntie_bot/maintenance.hiauntie.com
-python3 -m hiauntie_py.toot --account verbose --msg "HiAuntie.com 即將進行系統維護，預計需時一小時。系統會間歇停止運作，用戶可能會受到影響。請避免在維護期間發文。不便之處，敬請原諒。"
+python3 -m hiauntie_py.toot --account announcement --msg "HiAuntie.com 即將進行系統維護，預計需時一小時。系統會間歇停止運作，用戶可能會受到影響。請避免在維護期間發文。不便之處，敬請原諒。"
 
 echo "PQHGVWPD maintenance_toot_warning toot end"
