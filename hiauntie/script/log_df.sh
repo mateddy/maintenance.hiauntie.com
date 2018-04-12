09301adc28cfab74183935efc955a671380cc6bd
Luzi82 <luzi82@gmail.com>
2018-04-12 09:17:29 +0800

    show total disk instead of available

--- hiauntie/script/log_df.sh
+++ hiauntie/script/log_df.sh
@@ -4,7 +4,7 @@ set -e
 
 TIMESTAMP_FULL=`date +%Y%m%d-%H%M%S`
 
-DF_VDA2_STAT=`df -BM | grep vda2 | awk '{ print $3 " / " $4 " = " $5 }'`
+DF_VDA2_STAT=`df -BM | grep vda2 | awk '{ print $3 " / " $2 " = " $5 }'`
 
 cd /home/hiauntie_bot/maintenance.hiauntie.com
 python3 -m hiauntie_py.toot --account verbose --msg "HIVKXVHV disk usage ${TIMESTAMP_FULL} ${DF_VDA2_STAT}"
